d77e4c1f939392842649fc9121a302b07faaa1b8
khiembimaster <masterkhiembi@gmail.com>
2023-07-31 19:13:06 +0700

    add logout and register views

--- core/views.py
+++ core/views.py
@@ -1,31 +1,41 @@
+from typing import Any, Dict
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import Game
 # Create your views here.
-class GameList(ListView):
+class GameList(LoginRequiredMixin, ListView):
     model = Game
     context_object_name = 'games'
 
-class GameDetail(DetailView):
+class GameDetail(LoginRequiredMixin, DetailView):
     model = Game
     context_object_name = 'game'
     template_name = 'core/game.html'
 
-class GamePublish(CreateView):
+class GamePublish(LoginRequiredMixin, CreateView):
+    model = Game
+    # fields = '__all__'
+    fields = ['name', 'genre','description','iframe','thumnail']
+    success_url = reverse_lazy('games')
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.user = self.request.user
+        return super(GamePublish, self).form_valid(form)
+
+class GameUpdate(LoginRequiredMixin, UpdateView):
     model = Game
     fields = '__all__'
     success_url = reverse_lazy('games')
 
-class GameUpdate(UpdateView):
-    model = Game
-    fields = '__all__'
-    success_url = reverse_lazy('games')
-
-class GameDelete(DeleteView):
+class GameDelete(LoginRequiredMixin, DeleteView):
     model = Game
     context_object_name = 'game'
     success_url = reverse_lazy('games')
--- user/views.py
+++ user/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
 class CustomLoginView(LoginView):
     template_name = 'user/login.html'
@@ -10,3 +15,20 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('games')
+
+class RegisterView(FormView):
+    template_name = 'user/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('games')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('games')
+        return super(RegisterView, self).get(*args, **kwargs)
