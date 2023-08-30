6cb10261370525a8575076a8993e1009d3cecdcd
khiem <nhkhiem21@clc.fitus.edu.vn>
2023-08-31 02:28:57 +0700

    add default storage

--- y8plusplus/settings.py
+++ y8plusplus/settings.py
@@ -133,10 +133,14 @@ MEDIA_ROOT = BASE_DIR / 'static/media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
     # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
