5f3b0dba6379551eef1301afed40c5b65c06ba8e
khiem <nhkhiem21@clc.fitus.edu.vn>
2023-08-31 10:57:35 +0700

    clear old static folder

--- y8plusplus/settings.py
+++ y8plusplus/settings.py
@@ -127,9 +127,9 @@ LOGIN_URL = 'login'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 MEDIA_ROOT = BASE_DIR / 'static/media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
